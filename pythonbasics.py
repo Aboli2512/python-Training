b35aa7f6800c326de82cef40c299a5819239d5d1
LAPTOP-4094FFC7\HP <abolichaudhari25@gmail.com>
2025-06-14 18:40:51 +0530

    syntax, variables

--- pythonbasics.py
+++ pythonbasics.py
@@ -1,3 +1,17 @@
-print("hello") 
+print("Hello World")
+# define new variable
+newVar1 = "Hello"
+print(newVar1)
 
+#python indentation
+if 5>2:
+    print("five is greater than 2")
+    print("Hello Saanvi")
+
+    #python variables
+    firstVar = 5
+    secondVar = '5'
+    print(firstVar , secondVar)
+    print(type(firstVar) , type(secondVar))
+    print(firstVar*5 , secondVar*5)
 
