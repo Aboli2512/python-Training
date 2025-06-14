8c03d12a3c0ff3736fced59c34aafe0d7d19c080
LAPTOP-4094FFC7\HP <abolichaudhari25@gmail.com>
2025-06-14 19:01:06 +0530

    casting,type,single and double quotes, case sensitive

--- pythonbasics.py
+++ pythonbasics.py
@@ -15,3 +15,35 @@ if 5>2:
     print(type(firstVar) , type(secondVar))
     print(firstVar*5 , secondVar*5)
 
+    #This is a comment
+    #written in
+    #more than one line
+
+    #casting
+    var1 = str(3)
+    var2 = int(3)
+    var3 = float(3)
+    print(var1 , var2 , var3)
+
+    #type
+    var1 = int(5)
+    var2 = "Saanvi"
+    print(type(var1))
+    print(type(var2))
+    print(var1)
+    print(var2)
+
+    #single and double quotes
+    var1 = "john"
+    var2 = 'john'
+    print(var1, var2)
+    #case sensitive
+    var1 = 4 
+    var2 = "SAANVI"
+    print(var1 , var2)
+    print(var1)
+    print(var2)
+
+
+
+    
