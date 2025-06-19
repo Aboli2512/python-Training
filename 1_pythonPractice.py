94fd004a560d17fd3bc7c635e62676962fdfba12
LAPTOP-4094FFC7\HP <abolichaudhari25@gmail.com>
2025-06-19 12:01:10 +0530

    practise1

---
+++ 1_pythonPractice.py
@@ -0,0 +1,25 @@
+# Question 1
+# print twinkle twinkle little star poem.
+print(""" Twinkle, twinkle, little star,
+How I wonder what you are,
+Up above the world so high,
+Like a diamond in the sky,
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+Then the traveler in the dark,
+Thanks you for your tiny spark,
+He couldn't see which way to go,
+If you did not twinkle so. """)
+
+print ( ">>>>>>>>>>>>>>>>>>>" ) 
+
+# Question 2
+# print the table of table using repl and pyhton
+
+for i in range(0 , 11):
+    print(5 * i , '\n')
+print(">>>>>>>>>>>>>>>")   
+   
+
